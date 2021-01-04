b62e82cedf78083f93664f54adeec1b45bbc8bd2
Jessica Ho <hwyann@gmail.com>
2021-01-04 14:25:16 +0900

    page layout

---
+++ src/components/GifList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class GifList extends Component {
+  render() {
+    return (
+      <div　className="gif-list">gif list</div>
+    );
+  }
+}
+
+export default GifList;
---
+++ src/components/SearchForm.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SearchForm extends Component {
+  render() {
+    return (
+      <div　className="form-search">search form</div>
+    );
+  }
+}
+
+export default SearchForm;
